2321ea6139d759152a4a87aa1e67742dda1dcfb8
blbaumg <lynnbaumgardner01@gmail.com>
2024-08-20 20:23:12 -0400

    first commit

---
+++ aboutme-may2024/script.js
@@ -0,0 +1,10 @@
+
+window.console.log("Future developer!");
+
+const h1Element = window.document.querySelector("h1");
+
+if (h1Element) {
+    h1Element.style.color = "white";
+}
+
+const clickMeButtonElement = window.document.querySelector("#click-me-button");
